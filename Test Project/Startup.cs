504f1e0646f685fbeff85e74ad2edd94c2221337
Claxon <tc513713@ohio.edu>
2019-10-11 17:13:41 -0400

    Add project files.

---
+++ Test Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test_Project.Startup))]
+namespace Test_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
