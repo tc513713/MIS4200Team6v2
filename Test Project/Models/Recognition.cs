769b7d50e42b6e7834351e53a357b026e1820da6
Kyle Weese <kw258215@ohio.edu>
2019-12-06 16:16:15 -0500

    Please dear God

--- Test Project/Models/Recognition.cs
+++ Test Project/Models/Recognition.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
 
 namespace Test_Project.Models
 {
     public class TestCoreValues
     {
-        public int ID { get; set; }
+        [Key]
+
+        public int recognitionID { get; set; }
+
+
         [Display(Name = "Core value recognized")]
         public CoreValue award { get; set; }
         [Display(Name = "ID of Person giving the recognition")]
@@ -14,11 +20,16 @@ namespace Test_Project.Models
         public DateTime recognizationDate { get; set; }
         public enum CoreValue
         {
+            [Display(Name = "Please Choose a Value")]
+            Select = 0,
             Excellence = 1,
             Integrity = 2,
             Stewardship = 3,
             Innovate = 4,
             Balance = 5
         }
+
+        public GUID ID { get; set; }
+        public virtual Employee Employee { get; set; }
     }
 }
